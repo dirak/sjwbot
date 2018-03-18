cd8a044d426f32c76827266a4ad1b4a3fac87cb1
Yuku <traxetren@gmail.com>
2018-03-18 13:40:01 +0100

    joke shouldn't match messages with links anymore

--- chat/joke.js
+++ chat/joke.js
@@ -1,8 +1,8 @@
 module.exports = {
 	chat : function(client, channel, message, userstate) {
 		if(typeof message != "undefined" && typeof userstate != "undefined") {
-			if(message.match(/^tell me a joke about/i)) {
-				var subject = message.replace(/^tell me a joke about/i, '');
+			if(message.match(/^tell me a joke about/i) && !message.match(/(http|\.\S)/i)) {
+				var subject = message.replace(/^tell me a joke about\s*/i, '');
 				var user = userstate['display-name'];
 				client.say(channel, `Did you hear about ${subject}, ${user}? It was destroyed.`);
 			}
